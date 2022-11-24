5e88b338f4a44543ca55d8b6ff9f9c2221f493f6
Igor Yasinsky <iyasinskyy@gmail.com>
2022-11-24 11:11:49 +0200

    update modal

--- js/modal.js
+++ js/modal.js
@@ -5,12 +5,19 @@
     modal: document.querySelector('[data-backdrop]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-  refs.modal.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', (e)=>toggleModal(e));
+  refs.closeModalBtn.addEventListener('click', (e)=>toggleModal(e));
+  refs.modal.addEventListener('click', (e)=>toggleModal(e));
   
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+  function toggleModal(e) {
+    if (e.currentTarget.nodeName!=="BUTTON") {
+      if (e.currentTarget === e.target) {
+        refs.modal.classList.toggle('is-hidden');
+      }
+    } else {
+      refs.modal.classList.toggle('is-hidden');
+    }
+   
   }
 })();
